0978d516ef1fc9f376b6d34a181678079aa43d06
AkT-Singh101 <126188221+AkT-Singh101@users.noreply.github.com>
2024-06-30 23:36:45 +0530

    Create Project1.sol

---
+++ Project1.sol
@@ -0,0 +1,18 @@
+pragma solidity ^0.8.0;
+
+contract TestContract {
+    uint public divident;
+    uint public divisor;
+    uint public result;
+
+    function set(uint D,uint d) public {
+        require(D >= 0, "Value must not be negative.");
+        assert(D<1000 && d<100);
+        if (d <= 0) {
+            revert("Divisor cannot be 0 or negative");
+        }
+        divident=D;
+        divisor=d;
+        result=divident/divisor;
+    }
+}
